971dd964c8d315a17a8cc5b39ab8776cd19e9e0d
Michael Jones <iammichaeljon27@gmail.com>
2022-07-29 18:22:23 +0400

    Destructure props

--- src/components/General.js
+++ src/components/General.js
@@ -2,44 +2,43 @@
 import React from "react";
 
 const General = (props) => {
-
+    const { handler, value } = props
     return (
-
         <div>
             <div>
                 <label htmlFor="name">Name</label>
                 <input type="text" id="name"
-                    value={props.value.name} onChange={props.handler} name="name"
+                    value={value.name} onChange={handler} name="name"
                     data-info="personal" />
             </div>
             <div>
                 <label htmlFor="email">EMAIL</label>
                 <input type="text" id="email"
-                    value={props.value.email} onChange={props.handler} name="email"
+                    value={value.email} onChange={handler} name="email"
                     data-info="personal" />
             </div>
             <div>
                 <label htmlFor="occupation">occupation</label>
                 <input type="text" id="occupation"
-                    value={props.value.occupation} onChange={props.handler} name="occupation"
+                    value={value.occupation} onChange={handler} name="occupation"
                     data-info="personal" />
             </div>
             <div>
                 <label htmlFor="phone">Phone Number</label>
                 <input type="text" id="phone"
-                    value={props.value.phone} onChange={props.handler} name="phone"
+                    value={value.phone} onChange={handler} name="phone"
                     data-info="personal" />
             </div>
             <div>
                 <label htmlFor="dob">Date of Birth</label>
                 <input type="text" id="dob"
-                    value={props.value.DOB} onChange={props.handler} name="DOB"
+                    value={value.DOB} onChange={handler} name="DOB"
                     data-info="personal" />
             </div>
             <div>
                 <label htmlFor="location">location</label>
                 <input type="text" id="location"
-                    value={props.value.location} onChange={props.handler} name="location"
+                    value={value.location} onChange={handler} name="location"
                     data-info="personal" />
             </div>
 
