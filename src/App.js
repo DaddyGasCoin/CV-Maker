3f978906e7e235f5810426cf8e1239389711fbab
Michael Jones <iammichaeljon27@gmail.com>
2022-07-29 12:15:57 +0400

    Render display and edit forms

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 
 import React, { Component } from "react";
+import Display from "./components/Display";
+import General from "./components/General";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      personal: { name: '', email: '', occupation: '', DOB: '', phone: '', location: '' },
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const type = target.dataset.info
+    const value = target.value;
+    const name = target.name;
+    this.setState({ [type]: { ...this.state[type], [name]: value } });
+
+
+  }
+
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div className="container">
+        <div className="form-enter">
+          <General handler={this.handleChange} value={this.state.personal} />
+        </div>
+        <div className="form-view">
+          <Display value={this.state} />
+        </div>
+      </div>
+    );
   }
 }
 
