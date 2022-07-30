5c31185da7a11702ee951539ef7cfddcb432c01d
Michael Jones <iammichaeljon27@gmail.com>
2022-07-30 13:14:47 +0400

    Refactor code to use infodetail component

--- src/components/General.js
+++ src/components/General.js
@@ -1,47 +1,16 @@
 
 import React from "react";
+import InfoDetail from "./InfoDetail";
 
 const General = (props) => {
-    const { handler, value } = props
     return (
         <div>
-            <div>
-                <label htmlFor="name">Name</label>
-                <input type="text" id="name"
-                    value={value.name} onChange={handler} name="name"
-                    data-info="personal" />
-            </div>
-            <div>
-                <label htmlFor="email">EMAIL</label>
-                <input type="text" id="email"
-                    value={value.email} onChange={handler} name="email"
-                    data-info="personal" />
-            </div>
-            <div>
-                <label htmlFor="occupation">occupation</label>
-                <input type="text" id="occupation"
-                    value={value.occupation} onChange={handler} name="occupation"
-                    data-info="personal" />
-            </div>
-            <div>
-                <label htmlFor="phone">Phone Number</label>
-                <input type="text" id="phone"
-                    value={value.phone} onChange={handler} name="phone"
-                    data-info="personal" />
-            </div>
-            <div>
-                <label htmlFor="dob">Date of Birth</label>
-                <input type="text" id="dob"
-                    value={value.DOB} onChange={handler} name="DOB"
-                    data-info="personal" />
-            </div>
-            <div>
-                <label htmlFor="location">location</label>
-                <input type="text" id="location"
-                    value={value.location} onChange={handler} name="location"
-                    data-info="personal" />
-            </div>
-
+            <InfoDetail main={props} name="name" group="personal" />
+            <InfoDetail main={props} name="email" group="personal" />
+            <InfoDetail main={props} name="occupation" group="personal" />
+            <InfoDetail main={props} name="DOB" group="personal" />
+            <InfoDetail main={props} name="phone" group="personal" />
+            <InfoDetail main={props} name="location" group="personal" />
         </div>
 
     )
